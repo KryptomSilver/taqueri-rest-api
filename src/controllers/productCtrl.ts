ee6a3c3aa1d67f4f49e2dca0941184e07f83b9fc
KryptomSilver <abelromeror763@gmail.com>
2022-02-22 07:02:49 -0600

    Added create product in controller and route

--- src/controllers/productCtrl.ts
+++ src/controllers/productCtrl.ts
@@ -1,2 +1,37 @@
-const productsCtrl = {};
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { getConnection } from "typeorm";
+import { Product } from "../models/Product";
+
+const productsCtrl = {
+  createProduct: async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { name, description } = req.body;
+      let connection = getConnection();
+      let productRepository = connection.getRepository(Product);
+      const existProduct = await productRepository.find({
+        where: {
+          name: name,
+        },
+      });
+      if (Object.keys(existProduct).length !== 0) {
+        return res.status(409).json({ message: "Product already exist!" });
+      }
+      let product = new Product();
+      product = {
+        description,
+        name,
+      };
+      await productRepository.save(product);
+      return res.status(201).json({ message: "Product created!" });
+    } catch (error: any) {
+      res.status(500).json({ message: "Ups.." });
+      console.log(error);
+    }
+  },
+};
 export default productsCtrl;
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,3 +1,13 @@
 import { Router } from "express";
+import { check } from "express-validator";
+import ProductCtrl from "../controllers/productCtrl";
 const router = Router();
+router.post(
+  "/",
+  [
+    check("name").notEmpty().isString(),
+    check("description").notEmpty().isString(),
+  ],
+  ProductCtrl.createProduct
+);
 export default router;
