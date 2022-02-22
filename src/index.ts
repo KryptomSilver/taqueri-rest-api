46ad2c86ab6137a01dacad29f226d556f4e19736
KryptomSilver <abelromeror763@gmail.com>
2022-02-22 06:56:10 -0600

    Create index app

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
+import routes from "./routes";
+
+// --- Config APP --- //
+
+createConnection();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// --- Routes --- //
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "REST API taqueria" });
+});
